9e26f2109ae12f833cf42c0fc90b55d0c22abc6d
harlo <harlo.holmes@gmail.com>
2015-10-11 17:47:12 +0000

    pin

---
+++ README.md
@@ -0,0 +1,13 @@
+#	Is He Cheating?
+
+##	Instructions
+
+###	Power-on
+
+```
+screen
+startx
+[ctrl-ad]
+cd ~/IsHeCheating
+python is_he_cheating.py --start
+```
--- is_he_cheating.py
+++ is_he_cheating.py
@@ -3,7 +3,8 @@ from sys import argv, exit
 from time import time, sleep
 
 from core.api import MPServerAPI
-from core.vars import DEFAULT_TELEPHONE_GPIO
+from core.video_pad import MPVideoPad
+from core.vars import DEFAULT_TELEPHONE_GPIO, BASE_DIR
 
 KEY_MAP = {
 	'demo_main_menu' : [
@@ -28,17 +29,25 @@ KEY_MAP = {
 	]
 }
 
-class IsHeCheating(MPServerAPI):
+class IsHeCheating(MPServerAPI, MPVideoPad):
 	def __init__(self):
 		MPServerAPI.__init__(self)
 
 		self.gpio_mappings = DEFAULT_TELEPHONE_GPIO
 		self.key_mappings = KEY_MAP
 
+		self.conf['d_files']['vid'] = {
+			'log' : self.conf['d_files']['module']['log'],
+			'pid' : os.path.join(BASE_DIR, ".monitor", "video_pad.pid.txt")
+		}
+
+		MPVideoPad.__init__(self)
+
 		logging.basicConfig(filename=self.conf['d_files']['module']['log'], level=logging.DEBUG)
 
 	def run_script(self):
 		super(IsHeCheating, self).run_script()
+		self.play_video("VID_20140815_143633.mp4")
 		self.route_loop('demo_main_menu')
 
 if __name__ == "__main__":
